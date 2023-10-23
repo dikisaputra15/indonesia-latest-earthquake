6fb23b70ba5ee27fa0c899bac5b92e4d47b82379
dikisaputra15 <mbpretina@MBPRetinas-MacBook-Pro.local>
2023-10-23 22:02:12 +0700

    coba publish pypi

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # indonesia-latest-earthquake
 the data of this project from BMKG | Meteorological, Climatological, and Geophysical Agency, latest earthquake
+
+# How it works
+this package will scrape from [BMKG] (https://bmkg.go.id) to get latest quake happened in indonesia 
+
+This package will use beautifulsoup4 and requests, to produce output in the form of JSON that is ready to be used in web or mobile applications
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+   name="indonesia-earthquake",
+   version="0.1",
+   author="Diki Saputra",
+   author_email="dikimistak@gmail.com",
+   description="the data of this project from BMKG | Meteorological, Climatological, and Geophysical Agency, "
+               "latest earthquake",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url="https://github.com/dikisaputra15/indonesia-latest-earthquake",
+   project_urls={
+       "website" : "selaicoding.com"
+   },
+   scripts=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: GNU General Public License v3 (gplv3)e",
+            "Operating System :: OS Independent",
+            "Development Status :: 5 - Production Stable"
+           ],
+   packages=setuptools.find_packages(),
+   python_requires=">=3.6",
+
+)
